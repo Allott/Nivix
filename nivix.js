ef0ed5728528f58b434ad8eef083ccd69f29665f
Daniel Allott <36487931+Allott@users.noreply.github.com>
2019-12-05 16:46:36 +0000

    Add files via upload

--- nivix.js
+++ nivix.js
@@ -7,30 +7,72 @@ function constructReg(re) {//to save me typing out all the diffrent characters a
     return regex;
 } 
 
-
-var ninety = constructReg("(c|e|i|l|m|n|o|r|v)");
-
-console.log(ninety);
-
-var str = "daniel allott jamie hannah niv oovvll"
-
-console.log(str.match(ninety))
-
-
-
-
-
-//take in input
 var myArgs = process.argv;
+var str = "";
 
 if (myArgs.length === 2) {
     console.log("no input");
+    return 0;
 }
-else
-{
-    var s = "";
+else {
+    
     for (var i = 2; i < myArgs.length; i++) {
-        s += " " + myArgs[i];
+        str += " " + myArgs[i];
     }
-    console.log(s);
 }
+
+var ninety = constructReg("(c|e|i|m|n|o|r|v|w|x|z)");
+var output = ""
+var slicestart = 0;
+
+while ((found = ninety.exec(str)) !== null) {
+    var rotate = found[0];
+    var rotate2 = "";
+    for (var i = 0; i < rotate.length; i++) {
+        switch (rotate.charAt(i)) {//better solution in future
+            case "c":
+                rotate2 += "U";
+                break;
+            case "e":
+                rotate2 += "W";
+                break;
+            case "i":
+                rotate2 += "-";
+                break;
+            case "m":
+                rotate2 += "E";
+                break;
+            case "n":
+                rotate2 += "Z";
+                break;
+            case "o":
+                rotate2 += "O";
+                break;
+            case "r":
+                rotate2 += "L";
+                break;
+            case "v":
+                rotate2 += ">";
+                break;
+            case "w":
+                rotate2 += "3";
+                break;
+            case "x":
+                rotate2 += "X";
+                break;
+            case "z":
+                rotate2 += "N";
+                break;
+            default:
+        }
+    }
+
+    output += str.slice(slicestart, ninety.lastIndex - rotate.length).toUpperCase();
+    output += "(" + rotate2 + ")90";
+    slicestart = ninety.lastIndex;
+}
+
+output += str.slice(slicestart, str.length) + " = 1";
+console.log(output)
+
+
